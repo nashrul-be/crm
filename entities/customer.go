e22040dd67dc2dfdaf3bb809b2f4435d6c9f71e3
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-01 21:10:08 +0700

    add json tag

--- entities/customer.go
+++ entities/customer.go
@@ -1,11 +1,13 @@
 package entities
 
+import "time"
+
 type Customer struct {
-	ID        uint   `json:"ID,omitempty"`
-	FirstName string `json:"firstName,omitempty"`
-	LastName  string `json:"lastName,omitempty"`
-	Email     string `json:"email,omitempty"`
-	Avatar    string `json:"avatar,omitempty"`
-	CreatedAt string `json:"createdAt,omitempty"`
-	UpdatedAt string `json:"updatedAt,omitempty"`
+	ID        uint      `json:"ID,omitempty"`
+	FirstName string    `json:"firstName,omitempty"`
+	LastName  string    `json:"lastName,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Avatar    string    `json:"avatar,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+	UpdatedAt time.Time `json:"updatedAt,omitempty"`
 }
