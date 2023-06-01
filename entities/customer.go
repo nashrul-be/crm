3dd75d465a798d0870b33cc230f0ead8675623bd
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-01 21:34:30 +0700

    remove json tag from customer entity

--- entities/customer.go
+++ entities/customer.go
@@ -3,11 +3,11 @@ package entities
 import "time"
 
 type Customer struct {
-	ID        uint      `json:"ID,omitempty"`
-	FirstName string    `json:"firstName,omitempty"`
-	LastName  string    `json:"lastName,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Avatar    string    `json:"avatar,omitempty"`
-	CreatedAt time.Time `json:"createdAt,omitempty"`
-	UpdatedAt time.Time `json:"updatedAt,omitempty"`
+	ID        uint
+	FirstName string
+	LastName  string
+	Email     string
+	Avatar    string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
