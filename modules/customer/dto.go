f7251d0eb8a690f82e6ec5e4e387e9e14c773112
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-01 21:10:24 +0700

    add json tag to customer dto

--- modules/customer/dto.go
+++ modules/customer/dto.go
@@ -1,15 +1,15 @@
 package customer
 
 type CreateRequest struct {
-	FirstName string `binding:"required,alpha"`
-	LastName  string `binding:"required,alpha"`
-	Email     string `binding:"required,email"`
-	Avatar    string
+	FirstName string `binding:"required,alpha" json:"first_name"`
+	LastName  string `binding:"required,alpha" json:"last_name"`
+	Email     string `binding:"required,email" json:"email"`
+	Avatar    string `json:"avatar"`
 }
 
 type Representation struct {
-	FirstName string
-	LastName  string
-	Email     string
-	Avatar    string
+	FirstName string `json:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Avatar    string `json:"avatar,omitempty"`
 }
