c454dec87d2b0a4b0cb5e92ff799f6580abe053a
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-01 19:52:15 +0700

    add base response and default response for most http code

--- dto/response.go
+++ dto/response.go
@@ -1,7 +1,46 @@
 package dto
 
-type Response struct {
+import "net/http"
+
+type BaseResponse struct {
 	Code    int
 	Message string
 	Data    any
+}
+
+func ErrorNotFound(msgErr string) BaseResponse {
+	return BaseResponse{
+		Code:    http.StatusNotFound,
+		Message: msgErr,
+	}
+}
+
+func ErrorBadRequest(msgErr string) BaseResponse {
+	return BaseResponse{
+		Code:    http.StatusBadRequest,
+		Message: msgErr,
+	}
+}
+
+func ErrorInternalServerError() BaseResponse {
+	return BaseResponse{
+		Code:    http.StatusInternalServerError,
+		Message: "Oops, something wrong!",
+	}
+}
+
+func Success(msg string, data any) BaseResponse {
+	return BaseResponse{
+		Code:    http.StatusOK,
+		Message: msg,
+		Data:    data,
+	}
+}
+
+func Created(msg string, data any) BaseResponse {
+	return BaseResponse{
+		Code:    http.StatusCreated,
+		Message: msg,
+		Data:    data,
+	}
 }
