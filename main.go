2ef0611b788be0626e15358c47bdecad7033004a
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-02 20:46:35 +0700

    add route for register approval

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"nashrul-be/crm/modules/actor"
 	"nashrul-be/crm/modules/customer"
+	register_approval "nashrul-be/crm/modules/register-approval"
 	"nashrul-be/crm/repositories"
 	"nashrul-be/crm/utils/db"
 )
@@ -26,12 +27,16 @@ func main() {
 	userRepo := repositories.NewCustomerRepository(dbConn)
 	actorRepo := repositories.NewActorRepository(dbConn)
 	roleRepo := repositories.NewRoleRepository(dbConn)
+	approvalRepo := repositories.NewRegisterApprovalRepository(dbConn)
 
 	userRoute := customer.NewCustomerRoute(userRepo)
 	userRoute.Handle(engine)
 
-	actorRoute := actor.NewActorRoute(actorRepo, roleRepo)
+	actorRoute := actor.NewActorRoute(actorRepo, roleRepo, approvalRepo)
 	actorRoute.Handle(engine)
+
+	approveRoute := register_approval.NewApprovalRoute(actorRepo, approvalRepo)
+	approveRoute.Handle(engine)
 
 	if err := engine.Run(); err != nil {
 		panic(err.Error())
