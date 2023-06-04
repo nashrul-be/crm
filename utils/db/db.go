6564adfb8413d6bc8515e8b244e1590c77d64ec9
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-04 21:23:56 +0700

    read db config from .env

--- utils/db/db.go
+++ utils/db/db.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 type Config struct {
@@ -17,6 +19,18 @@ type Config struct {
 func Connect(config Config) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.User, config.Password,
 		config.Host, config.Port, config.DBName)
+	return ConnectWithDSN(dsn)
+}
+
+func DefaultConnection() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	return ConnectWithDSN(dsn)
+}
+
+func ConnectWithDSN(dsn string) (*gorm.DB, error) {
+	log.Println(dsn)
 	dbConn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
